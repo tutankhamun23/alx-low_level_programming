ad1af68c18b867d14a9e1c180f2954b0c23278ab
tutankhamun23 <gene.elvis10@email.com>
2022-12-13 17:08:32 -0800

    updated

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,15 +6,15 @@
  */
 int main(void)
 {
-	char str[] = "Holberton\n";
-	int i = 0;
+char str[] = "_putchar\n";
+int i = 0;
 
-	while (str[i] != '\0')
-		{
-		char c = str[i];
-
-		_putchar(c);
-		i++;
-		}
-	return (0);
+while (str[i] != '\0')
+	{
+	char c = str[i];
+	
+	_putchar(c);
+	i++;
+	}
+return (0);
 }
