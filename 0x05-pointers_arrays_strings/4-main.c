cb210129d1a7ad37aa10c05bee36c39cb8e03bb3
tutankhamun23 <gene.elvis10@email.com>
2022-12-20 04:42:27 -0800

    a function that prints a string, in reverse, followed by a new line.

---
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * prints reverse
+ */
+
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	print_rev(str);
+	return (0);
+}
